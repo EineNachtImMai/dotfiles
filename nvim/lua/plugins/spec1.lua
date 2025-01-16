16a9c1c65867973093b7869081c8b25afbbbe394
EineNachtImMai <titouan.cazaubieilh@gmail.com>
2025-01-16 10:27:37 +0100

    Added additional plugins for git integration because I'm going on a multi-person project soon.
    Added lazygit.nvim, gistigns.nvim, diffview.nvim and nvim-neoclip.lua.

--- nvim/lua/plugins/spec1.lua
+++ nvim/lua/plugins/spec1.lua
@@ -1,5 +1,5 @@
 return {
-  -- NOTE:  keyboard shortcuts using space + key, shows uses and possible following keys.
+	-- NOTE:  keyboard shortcuts using space + key, shows uses and possible following keys.
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
@@ -15,7 +15,7 @@ return {
 		},
 	},
 
-  -- NOTE: lunarvim color scheme based on TokyoNight, my personal fav scheme.
+	-- NOTE: lunarvim color scheme based on TokyoNight, my personal fav scheme.
 	{
 		"lunarvim/lunar.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -26,19 +26,19 @@ return {
 		end,
 	},
 
-  -- NOTE: tabs in neovim for the different open buffers.
+	-- NOTE: tabs in neovim for the different open buffers.
 	{
 		"akinsho/bufferline.nvim",
 		dependencies = "nvim-tree/nvim-web-devicons",
 	},
 
-  -- NOTE: status line at the bottom of the screen.
+	-- NOTE: status line at the bottom of the screen.
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 
-  -- NOTE: file browser tab for neovim.
+	-- NOTE: file browser tab for neovim.
 	{
 		"nvim-tree/nvim-tree.lua",
 		lazy = true,
@@ -47,7 +47,7 @@ return {
 		},
 	},
 
-  -- NOTE: fuzzy finder for neovim. Crazy powerful, and has integrations for a lot of plugins.
+	-- NOTE: fuzzy finder for neovim. Crazy powerful, and has integrations for a lot of plugins.
 	{
 		"nvim-telescope/telescope.nvim",
 		lazy = true,
@@ -56,18 +56,18 @@ return {
 		},
 	},
 
-  -- NOTE: dashboard for nvim.
+	-- NOTE: dashboard for nvim.
 	{
 		"goolord/alpha-nvim",
 		lazy = true,
 	},
 
-  -- NOTE: syntax highlighting using tree-sitter.
+	-- NOTE: syntax highlighting using tree-sitter.
 	{
 		"nvim-treesitter/nvim-treesitter",
 	},
 
-  -- NOTE: autocompletion from snippets.
+	-- NOTE: autocompletion from snippets.
 	{
 		"hrsh7th/nvim-cmp",
 		opts = function(_, opts)
@@ -86,30 +86,30 @@ return {
 	}, -- Required
 	{ "rafamadriz/friendly-snippets" },
 
-  -- NOTE: nvim-cmp source for neovim's built-in language server client.
+	-- NOTE: nvim-cmp source for neovim's built-in language server client.
 	{ "hrsh7th/cmp-nvim-lsp" },
 
-  -- NOTE: allows nvim-cmp to use the buffer (aka take into account what you already typed to suggest it again)
+	-- NOTE: allows nvim-cmp to use the buffer (aka take into account what you already typed to suggest it again)
 	{ "hrsh7th/cmp-buffer" },
 
-  -- NOTE: allows nvim-cmp to help with suggesting paths.
+	-- NOTE: allows nvim-cmp to help with suggesting paths.
 	{ "hrsh7th/cmp-path" },
 
-  -- NOTE: luasnip completion source for nvim-cmp.
+	-- NOTE: luasnip completion source for nvim-cmp.
 	{ "saadparwaiz1/cmp_luasnip" },
 
-  -- NOTE: nvim-cmp source for neovim Lua API.
+	-- NOTE: nvim-cmp source for neovim Lua API.
 	{ "hrsh7th/cmp-nvim-lua" },
 
-  -- NOTE: allows nvim-cmp to autocomplete your nvim commands too.
+	-- NOTE: allows nvim-cmp to autocomplete your nvim commands too.
 	{ "hrsh7th/cmp-cmdline" },
 
-  -- NOTE: sane defaults for the nvim lsp.
+	-- NOTE: sane defaults for the nvim lsp.
 	{
 		"neovim/nvim-lspconfig",
 	},
 
-  -- NOTE: configures LuaLS to recognize vim config variables.
+	-- NOTE: configures LuaLS to recognize vim config variables.
 	{
 		"folke/lazydev.nvim",
 		ft = "lua", -- only load on lua files
@@ -122,19 +122,19 @@ return {
 		},
 	},
 
-  -- NOTE: terminal integration into neovim.
+	-- NOTE: terminal integration into neovim.
 	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		config = true,
 	},
 
-  -- NOTE: automatically inserts the corresponding closing character ( e.g. ")" when typing "(" ). Can do other great stuff, but for now I only use it for that.
+	-- NOTE: automatically inserts the corresponding closing character ( e.g. ")" when typing "(" ). Can do other great stuff, but for now I only use it for that.
 	{
 		"windwp/nvim-autopairs",
 	},
 
-  -- NOTE: Never lose your work because of a stupid undo + write again. saves your undo's as a tree so you can freely browse and access them.
+	-- NOTE: Never lose your work because of a stupid undo + write again. saves your undo's as a tree so you can freely browse and access them.
 	{
 		"jiaoshijie/undotree",
 		dependencies = {
@@ -142,13 +142,13 @@ return {
 		},
 	},
 
-  -- NOTE: multiple cursor in nvim.
+	-- NOTE: multiple cursor in nvim.
 	{
 		"mg979/vim-visual-multi",
 		branch = "master",
 	},
 
-  -- NOTE: Nicer UI for notifications, commands, password input, ...
+	-- NOTE: Nicer UI for notifications, commands, password input, ...
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
@@ -159,34 +159,34 @@ return {
 		},
 	},
 
-  -- NOTE: Really useful plugin, automatically comments one or multiple lines, using the correct characters depending on the language.
+	-- NOTE: Really useful plugin, automatically comments one or multiple lines, using the correct characters depending on the language.
 	{ "numToStr/Comment.nvim" },
 
-  -- NOTE: Nicer linting, with the possibility of only displaying the message when you're on the error's line.
+	-- NOTE: Nicer linting, with the possibility of only displaying the message when you're on the error's line.
 	{
 		"rachartier/tiny-inline-diagnostic.nvim",
 		event = "VeryLazy", -- Or `LspAttach`
 	},
 
-  -- NOTE: Project management in nvim: detects project directories (for example containing a .git file). Integrates very well with Telescope.
+	-- NOTE: Project management in nvim: detects project directories (for example containing a .git file). Integrates very well with Telescope.
 	{ "ahmedkhalf/project.nvim" },
 
-  -- NOTE: Recursively search a directory for a regex.
+	-- NOTE: Recursively search a directory for a regex.
 	{ "BurntSushi/ripgrep" },
 
-  -- NOTE: Forgot to use sudo for a file for which you don't have write permissions? Suda allows you to type in your sudo password and edit it anyway.
+	-- NOTE: Forgot to use sudo for a file for which you don't have write permissions? Suda allows you to type in your sudo password and edit it anyway.
 	{ "lambdalisue/vim-suda" },
 
-  -- NOTE: good little plugin for practicing basic navigation.
+	-- NOTE: good little plugin for practicing basic navigation.
 	{ "ThePrimeagen/vim-be-good" },
 
-  -- NOTE: Helps integrate nvim into tmux.
+	-- NOTE: Helps integrate nvim into tmux.
 	{ "christoomey/vim-tmux-navigator", lazy = false },
 
-  -- NOTE: Vim syntax highlighting for .kbd files. I use kanata for a few remaps, so this comes handy every so often.
+	-- NOTE: Vim syntax highlighting for .kbd files. I use kanata for a few remaps, so this comes handy every so often.
 	{ "kmonad/kmonad-vim" },
 
-  -- NOTE: awesome plugin for editing and writing markdown. Renders it right into the editor, great for visualizing it better.
+	-- NOTE: awesome plugin for editing and writing markdown. Renders it right into the editor, great for visualizing it better.
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
@@ -195,15 +195,15 @@ return {
 		opts = {},
 	},
 
-  -- NOTE: I'm using it right now while writing this. Create and browse different comment tage: todo, note, hack... and highlights it accordingly.
+	-- NOTE: I'm using it right now while writing this. Create and browse different comment tage: todo, note, hack... and highlights it accordingly.
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
 	},
 
-  -- NOTE: Create and edit obsidian notes in neovim. I love nvim, but markdown is much more readable in obsidian, and some things like pdf annotations
-  -- just aren't possible without it.
+	-- NOTE: Create and edit obsidian notes in neovim. I love nvim, but markdown is much more readable in obsidian, and some things like pdf annotations
+	-- just aren't possible without it.
 	{
 		"epwalsh/obsidian.nvim",
 		version = "*", -- recommended, use latest release instead of latest commit
@@ -223,8 +223,8 @@ return {
 		},
 	},
 
-  -- NOTE: Leetcode challenges built right into neovim, meaning you can use the editor you're actually used to, without all the tedious copy-pasting for tests
-  -- and submitting
+	-- NOTE: Leetcode challenges built right into neovim, meaning you can use the editor you're actually used to, without all the tedious copy-pasting for tests
+	-- and submitting
 	{
 		"kawre/leetcode.nvim",
 		build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
@@ -233,24 +233,23 @@ return {
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
 		},
-		opts = {
-		},
+		opts = {},
 	},
 
-  -- NOTE: Forces you to use nvim more efficiently if possible (disables arrow keys, suggests shorter versions for some motions)
+	-- NOTE: Forces you to use nvim more efficiently if possible (disables arrow keys, suggests shorter versions for some motions)
 	{
 		"m4xshen/hardtime.nvim",
 		dependencies = { "MunifTanjim/nui.nvim" },
 		opts = {
 			disabled_keys = {
-        -- enable arrow keys in insert mode so you can still move around.
+				-- enable arrow keys in insert mode so you can still move around.
 				["<Left>"] = { "n", "v" },
 				["<Right>"] = { "n", "v" },
 				["<Up>"] = { "n", "v" },
 				["<Down>"] = { "n", "v" },
 			},
 			restricted_keys = {
-        -- disable the limit for successive direction key presses, cause sometimes i just don't know how low i wanna go.
+				-- disable the limit for successive direction key presses, cause sometimes i just don't know how low i wanna go.
 				["h"] = {},
 				["j"] = {},
 				["k"] = {},
@@ -259,8 +258,8 @@ return {
 		},
 	},
 
-  -- NOTE: Really useful plugin. Wanna enclose a word or selection with parentheses, brackets, or anything else that requres closing ? Then you
-  -- need this plugin.
+	-- NOTE: Really useful plugin. Wanna enclose a word or selection with parentheses, brackets, or anything else that requres closing ? Then you
+	-- need this plugin.
 	{
 		"kylechui/nvim-surround",
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
@@ -272,9 +271,64 @@ return {
 		end,
 	},
 
-  -- NOTE: Interface with code formatters so you can have a shortcut to run your favorite formatter.
+	-- NOTE: Interface with code formatters so you can have a shortcut to run your favorite formatter.
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+
+	-- NOTE: lazygit integration for nvim, a great gui for git.
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>Lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+
+	-- NOTE: diff view plugin for nvim, comparing with the latest modified file
+	{ "sindrets/diffview.nvim" },
+
+	-- NOTE: useful plugins for quickly bookmarking files
+	{
+		"otavioschwanck/arrow.nvim",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+		},
+		opts = {
+			show_icons = true,
+			leader_key = ";", -- Recommended to be a single key
+			buffer_leader_key = "m", -- Per Buffer Mappings
+			global_bookmarks = true, -- Allow the bookmarks to not be project-dependent
+		},
+	},
+
+  -- NOTE: great clipboard manager for neovim
+	{
+		"AckslD/nvim-neoclip.lua",
+		dependencies = {
+			{'nvim-telescope/telescope.nvim'},
+		},
+		config = function()
+			require("neoclip").setup()
+		end,
+	},
+
+  -- NOTE: gitsigns integration
   {
-    'stevearc/conform.nvim',
-    opts = {},
+    "lewis6991/gitsigns.nvim",
+    config = function ()
+      require("gitsigns").setup()
+    end
   },
 }
