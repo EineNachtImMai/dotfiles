99b04c30d77dd6c35a49183960d54e0694478c2e
EineNachtImMai <titouan.cazaubieilh@gmail.com>
2025-01-27 21:09:23 +0100

    Neovim changes
    Added Oil.nvim and "deprecated" NvimTree (different keybind, basically
    still there until i get used to Oil). Removed ToggleTerm in favor of a
    custom solution more tailored to my needs (automatic toggle, custom
    keybind to exit).

---
+++ nvim/lua/config/nekifock.lua
@@ -0,0 +1,4 @@
+return {
+    kitty_conf_path = '~/.config/kitty/kitty.conf',
+    borders = 'rounded', --available values are: 'rounded', 'single', 'double', 'shadow', 'solid', 'none'
+}
---
+++ nvim/plugin/test_plugin.lua
@@ -0,0 +1,11 @@
+local TerminalToggle = {}
+
+vim.api.nvim_create_user_command(
+  "Stuff",
+  function ()
+    print(vim.fn.input("Stuff: ", ""))
+  end,
+  {}
+)
+
+return TerminalToggle
