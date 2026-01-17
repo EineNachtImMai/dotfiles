abc43d8a47a845d772192d32b0b23f3cf4569cf6
EineNachtImMai <titouan.cazaubieilh@proton.me>
2026-01-17 15:02:30 +0100

    changed to mango!

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+require("tab-line")
+
+local wezterm = require("wezterm")
+
+local cfg = {
+	color_scheme = "Catppuccin Mocha",
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+	show_new_tab_button_in_tab_bar = false,
+	font = wezterm.font({ family = "DejaVu Sans Mono" }),
+	font_size = 14,
+	window_background_opacity = 0.2,
+
+	keys = {
+		{
+			key = "h",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTabRelative(-1),
+		},
+
+		{
+			key = "l",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTabRelative(1),
+		},
+	},
+}
+
+return cfg
